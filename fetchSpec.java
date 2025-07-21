4e5f95599560c67382294d7d2e007ff41b97192d
AtiqRehaman <atiqrehamanshaik@gmail.com>
2025-07-21 15:56:06 +0530

    Update fetchSpec.java

--- fetchSpec.java
+++ fetchSpec.java
@@ -3,35 +3,35 @@ import java.sql.*;
 import java.util.Scanner;
 public class fetchSpec
 {
-public static void main(String arg[])
-{
-try
-{
-	Scanner s = new Scanner(System.in);
-Class.forName("oracle.jdbc.driver.OracleDriver");
-Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","manager");
-System.out.print("Enter user id to see details:");
-String uid = s.next();
-
-String sql="select * from msusers where user_id='"+uid+"'";
-
-Statement stmt = con.createStatement();
-
-ResultSet rs = stmt.executeQuery(sql);
-while(rs.next())
-{
-System.out.println("User_id:"+rs.getString(1));
-System.out.println("User_name:"+rs.getString(2));
-System.out.println("password:"+rs.getString(3));
-System.out.println("Role:"+rs.getString(4));
-System.out.println();
-}
-
-con.close();
-}
-catch(Exception e)
-{
-	System.out.println(e);
-}
-}
+	public static void main(String arg[])
+	{
+		try
+		{
+			Scanner s = new Scanner(System.in);
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","manager");
+			System.out.print("Enter user id to see details:");
+			String uid = s.next();
+			
+			String sql="select * from msusers where user_id='"+uid+"'";
+			
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next())
+			{
+				System.out.println("User_id:"+rs.getString(1));
+				System.out.println("User_name:"+rs.getString(2));
+				System.out.println("password:"+rs.getString(3));
+				System.out.println("Role:"+rs.getString(4));
+				System.out.println();
+			}
+			
+			con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
 }
