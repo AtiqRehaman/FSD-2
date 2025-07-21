e8d402c86eab190e762e9be303e5d8f9de2db13e
AtiqRehaman <atiqrehamanshaik@gmail.com>
2025-07-21 15:52:36 +0530

    Lab-3

---
+++ fetchSpec.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+public class fetchSpec
+{
+public static void main(String arg[])
+{
+try
+{
+	Scanner s = new Scanner(System.in);
+Class.forName("oracle.jdbc.driver.OracleDriver");
+Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","manager");
+System.out.print("Enter user id to see details:");
+String uid = s.next();
+
+String sql="select * from msusers where user_id='"+uid+"'";
+
+Statement stmt = con.createStatement();
+
+ResultSet rs = stmt.executeQuery(sql);
+while(rs.next())
+{
+System.out.println("User_id:"+rs.getString(1));
+System.out.println("User_name:"+rs.getString(2));
+System.out.println("password:"+rs.getString(3));
+System.out.println("Role:"+rs.getString(4));
+System.out.println();
+}
+
+con.close();
+}
+catch(Exception e)
+{
+	System.out.println(e);
+}
+}
+}
